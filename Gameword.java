cc7ca90681e0ccf5fe53e127ded9047dee732f5f
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2018-11-01 22:08:49 -0400

    update1

--- Gameword.java
+++ Gameword.java
@@ -5,17 +5,33 @@
 class Gameword {
 	private double x, y, direction;
 
-	public static void anagram(String word){
+	public static void anagram(String word){//this is a recursion functions that will work as an anagram
 		anagram ("",word);
 	}
 	public static void anagram (String sofar, String left){
-		if(left.equals("")){
+		if(left.equals("")){// base case
 			System.out.println(sofar);
 		}
 		else{
 			for(int i = 0; i < left.length(); i++){
 				String next = left.substring(0,i) + left.substring(i + 1);
 				anagram(sofar + left.charAt(i),next);
+			}
+		}
+	}
+	
+	public static void permutation(String letter){ // this is a recursion function that works as a permutation
+		permutation("",letter);
+	}
+	
+	public static void permutation(String first, String letter){
+		int n = letter.length();
+		if(n==0){
+			System.out.println(first);
+		}
+		else{
+			for(int i = 0; i<n; i++){
+				permutation(first + letter.charAt(i), letter.substring(0,i) + letter.substring(i + 1, n));
 			}
 		}
 	}
--- GamewordTest.java
+++ GamewordTest.java
@@ -1,6 +1,7 @@
 class GamewordTest{
 	public static void main (String [] args){
 		Gameword Tester = new Gameword();
+		Tester.anagram("ab");
 		Tester.anagram("dog");
 	}
 }
